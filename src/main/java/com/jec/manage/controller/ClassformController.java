20e59238c1f2af37e2597526f91624335aba1694
d_d2017@163.com <d_d2017@163.com>
2019-12-16 15:05:49 +0800

    99

--- src/main/java/com/jec/manage/controller/ClassformController.java
+++ src/main/java/com/jec/manage/controller/ClassformController.java
@@ -1,6 +1,7 @@
 package com.jec.manage.controller;
 
 import com.jec.manage.service.ClassformService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ public class ClassformController {
     @Autowired
     ClassformService classformService;
     @RequestMapping("page/list")
-    public List selectList(String teachername){
+    public List selectList(@Param("teachername") String teachername){
         return classformService.selectList(teachername);
     }
 }
